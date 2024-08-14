1738e4e1636ca7cec5451b11e257b95b2370fced
jiajunliu0024 <jiajunliu0024@gmail.com>
2024-08-14 15:11:40 +1000

    update the requirements.txt

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from starlette.middleware.cors import CORSMiddleware
 from database import engine, Base
 from api.petrol_station_api import router as petrol_station_router
 from api.petrol_price_api import router as petrol_price_router
-from requsts.petrol_spy_request import read_local_spy_petrol_file
 
 app = FastAPI()
 
@@ -15,9 +14,9 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 app.include_router(petrol_station_router, prefix="/api/v1", tags=["Petrol Stations"])
 app.include_router(petrol_price_router, prefix="/api/v1", tags=["Petrol Price"])
+
 
 @app.on_event("startup")
 async def startup_event():
